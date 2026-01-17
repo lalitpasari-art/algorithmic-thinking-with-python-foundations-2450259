a4db83dd6a7c4f473997129a8a7e3c73f260765b
LP <lalit.pasari@gmail.com>
2026-01-17 19:33:46 +0000

    change coin, bug fixes

--- 05_02/my_solution.py
+++ 05_02/my_solution.py
@@ -1,10 +1,10 @@
 import functools as ft
 
-denominations = [100, 50, 20, 10, 5, 2, 1]
+denominations = [200, 100, 50, 20, 10, 5, 2, 1]
 
 @ft.lru_cache(maxsize=None)
 def make_change(target_amount):
-    if target_amount in denominations:
+    if target_amount in denominations or target_amount == 0:
         return 1
     min_coins = float('inf')
     for coin in denominations:
@@ -16,3 +16,5 @@ def make_change(target_amount):
 
 print(make_change(24))  # 3: 20p + 2p + 2p
 print(make_change(163))  # 5: £1 + 50p + 10p + 2p + 1p
+print(make_change(200))  # 5: £1 + 50p + 10p + 2p + 1p
+print(make_change(263))  # 5: £2 + 50p + 10p + 2p + 1p
